4f6bd925cfea3a74a5d46a19604fb01aef2c620a
Dan Jones <good.evil.genius@gmail.com>
2013-09-17 19:08:47 -0400

    Added error template and filled out index.php

---
+++ error.tpl.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+  <head><title><?=$http?:'Error'?></title></head>
+  <body><?=$error?></body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
 
 $version = "0.0.1";
+
+$url = $_GET['url'];
+$r = preg_match('#https?://(www\.)?([^/]+)/.*#', $url, $m);
+$service = $m[2];
+
+if (!$r) {
+  $http = "400 Bad Request";
+  $error = "Unrecognized URL";
+  header("HTTP/1.0 $http");
+  require_once('error.tpl.php');
+  exit;
+}
+
+if (!file_exists("engines/{$service}.php")) {
+  $http = "400 Bad Request";
+  $error = "Unrecognized service";
+  header("HTTP/1.0 $http");
+  require_once('error.tpl.php');
+  exit;
+}
+
+require_once("engines/{$service}.php");
+
+if (empty($tpl)) {
+  $http = "400 Bad Request";
+  $error = "Unable to parse page";
+  header("HTTP/1.0 $http");
+  require_once('error.tpl.php');
+  exit;
+}
+
+header("Content-type: application/rss+xml");
+require_once("mediarss.tpl.php");
